971b2b07d6daf5f01dbc983a59e6c64c0783a76e
houssem019 <houssemyousfi1999@gmail.com>
2022-02-24 17:13:38 +0100

    ...

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,4 +1,4 @@
-document.getElementById("cont2").style.display="none";
+
 $("form[name=signup_form").submit(function(e) {
   alert("Registred Successfully");
 
@@ -44,6 +44,7 @@ $("form[name=login_form").submit(function(e) {
   e.preventDefault();
 });
 var a=1;
+document.getElementById("cont2").style.display="none";
 function show_hide(){
   if(a==1){
     document.getElementById("cont2").style.display="inline";
