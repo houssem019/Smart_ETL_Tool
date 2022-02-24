ab0dcf0e453c7867f4acb86bf1d084866244f6cf
houssem019 <houssemyousfi1999@gmail.com>
2022-02-24 17:21:25 +0100

    upload done

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask, redirect, render_template,session
+from flask import Flask, redirect, render_template,session, url_for
 from functools import wraps
 import pymongo
+import os
+from werkzeug.utils import secure_filename
+from filter import *
 
 app=Flask(__name__)
 app.secret_key='intelligentetlprojecthoussemamanisupervisedbymaherheni'
+app.config['SECRET_KEY'] = 'houssemamani'
+app.config['UPLOAD_FOLDER'] = 'static/files'
 
 client=pymongo.MongoClient('localhost', 27017)
 db=client.etl
@@ -22,6 +27,7 @@ def login_required(f):
 
  #routes
 from user.routes import *
+from user.upload import UploadFileForm
 
 @app.route('/')
 def home():
@@ -31,15 +37,25 @@ def home():
 def register():
     return render_template('signup.html')
 
-@app.route('/extract')
+@app.route('/extract',methods=['GET','POST'])
 @login_required
 def extract():
-    return render_template('extract.html')
+    form=UploadFileForm()
+    if form.validate_on_submit():
+        file = form.file.data # First grab the file
+        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
+        return redirect(url_for('filter'))
+    return render_template('extract.html',form=form)
 
 @app.route('/transform')
 @login_required
 def transform():
     return render_template('transform.html')
+
+@app.route('/transform/filter',methods=['GET','POST'])
+@login_required
+def filter():
+    return render_template('filter.html',data=data)
 
 @app.route('/load')
 @login_required
