a20494e5bac24dacec89bb99cc31d925391d6c97
houssem019 <houssemyousfi1999@gmail.com>
2022-02-25 20:09:42 +0100

    ..

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
+from fileinput import filename
 from flask import Flask, redirect, render_template,session, url_for
 from functools import wraps
 import pymongo
+import gridfs
 import os
 from werkzeug.utils import secure_filename
-from filter import *
 
 app=Flask(__name__)
 app.secret_key='intelligentetlprojecthoussemamanisupervisedbymaherheni'
 app.config['SECRET_KEY'] = 'houssemamani'
 app.config['UPLOAD_FOLDER'] = 'static/files'
 
-client=pymongo.MongoClient('localhost', 27017)
+client=pymongo.MongoClient("mongodb+srv://houssem:05347268hO.@cluster0.e0gv8.mongodb.net/test")
 db=client.etl
-
+files=gridfs.GridFS(db)
 
 # Decorators
 def login_required(f):
@@ -42,8 +43,15 @@ def register():
 def extract():
     form=UploadFileForm()
     if form.validate_on_submit():
-        file = form.file.data # First grab the file
-        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
+        uploaded_file = form.file.data # First grab the file
+        files.put(uploaded_file,filename=uploaded_file.filename)
+        local_location="E:/P2M/ETL/static/files/"+uploaded_file.filename
+        downloaded_file=db.fs.files.find_one({'filename':uploaded_file.filename})
+        outputdata=files.get(downloaded_file['_id']).read()
+        output=open(local_location,"wb")
+        output.write(outputdata)
+        output.close()
+        # file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
         return redirect(url_for('filter'))
     return render_template('extract.html',form=form)
 
@@ -55,7 +63,7 @@ def transform():
 @app.route('/transform/filter',methods=['GET','POST'])
 @login_required
 def filter():
-    return render_template('filter.html',data=data)
+    return render_template('filter.html')
 
 @app.route('/load')
 @login_required
--- filter.py
+++ filter.py
@@ -1,4 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from app import db
+from app import files
+from flask import Flask
 
-data=pd.read_csv("static/files/Telecustclust.csv")
+
+downloaded_file=db.fs.files.find_one({})
+outputdata=files.get(downloaded_file['_id']).read()
+# file_path="static/files/"
+data=pd.DataFrame(list(outputdata))
+print(data.head())
